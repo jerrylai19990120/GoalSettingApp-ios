6ccd37dab308f3865e9f772054a305c37cf26044
jerry <jerrylai19990120@gmail.com>
2021-02-02 00:45:14 -0500

    Initial Commit

---
+++ Goal-Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goal-Manager
+//
+//  Created by Jerry Lai on 2021-02-02.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
